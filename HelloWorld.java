3bcb4ef9e8f348fac2cb34c5a39fb5f6551c389c
LuisNeriGe <99067859+LuisNeriGe@users.noreply.github.com>
2022-09-07 15:28:43 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ package Hello;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("----*****Hello World & Welcome to jenkins Training*****-----");
+		System.out.println("----*****Made Some Changes*****-----");
 	}
 }
